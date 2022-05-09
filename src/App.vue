03ba3ef11189d884c08cadf53365b7651af6a590
Tiago Matos <tiago@tiagomatos.com>
2022-05-09 15:21:35 +1000

    aula-3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader />
+
+    <img
+        alt="Vue logo"
+        src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        TheHeader
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .header {
+        background: #000;
+        color: #fff;
+    }
+</style>
