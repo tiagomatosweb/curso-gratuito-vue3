ff66ee2609f016a03351c65db91d5b4bd3acf226
Tiago Matos <tiago@tiagomatos.com>
2022-05-10 14:41:19 +1000

    aula-9

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,32 @@
 <template>
     <div>
-        <div>
-            One-way data binding <br>
-            Two-way data binding <br>
-            v-model -> formulários
+        <button @click.once="onClick">
+            Enviar
+        </button>
+        <br>
+
+        <div
+            @mouseover="onMouseOver"
+            @mouseout="onMouseOut"
+        >
+            Mouse over
         </div>
 
         <br><br>
 
-        <div>
-            <label>Nome</label> <br>
+        <form
+            action="https://google.com"
+            @submit.prevent="onSubmit"
+        >
             <input
-                v-model="name"
                 type="text"
-            > <br>
-            {{ name }}
-        </div>
+                @keyup.enter="onKeyUp"
+            >
+            <button type="submit">
+                Enviar
+            </button>
+        </form>
 
-        <br><br>
-
-        <div>
-            <label>Sports</label> <br>
-            <select v-model="sports">
-                <option value="">Escolha</option>
-                <option value="futebol">Futebol</option>
-                <option value="skate">Skate</option>
-                <option value="Tenis">Tenis</option>
-            </select> <br>
-            {{ sports }}
-        </div>
-
-        <br><br>
-
-        <div>
-            <label>Newsletter</label> <br>
-            <input
-                v-model="newsletter"
-                type="radio"
-                value="Sim"
-            > Sim
-
-            <input
-                v-model="newsletter"
-                type="radio"
-                value="Não"
-            > Não <br>
-            {{ newsletter }}
-        </div>
-
-        <br><br>
-
-        <div>
-            <label>Contrato</label> <br>
-            <input
-                v-model="contract"
-                type="checkbox"
-            > Aceita nosso termos... <br>
-
-              {{ contract }}
-        </div>
-
-        <br><br>
-
-        <div>
-            <label>Cores que você mais gosta</label> <br>
-            <input
-                v-model="colors"
-                type="checkbox"
-                value="Azul"
-            > Azul
-
-            <input
-                v-model="colors"
-                type="checkbox"
-                value="Amarelo"
-            > Amarelo <br>
-              {{ colors }}
-        </div>
     </div>
 </template>
 
@@ -85,11 +35,24 @@ export default {
     name: 'App',
     data() {
         return {
-            name: 'jon snow',
-            sports: 'futebol',
-            newsletter: 'Não',
-            contract: true,
-            colors: [],
+        }
+    },
+
+    methods: {
+        onClick($evt) {
+            console.log('click', $evt);
+        },
+        onMouseOver($evt) {
+            console.log('mouse over', $evt);
+        },
+        onMouseOut($evt) {
+            console.log('mouse out', $evt);
+        },
+        onSubmit($evt) {
+            console.log('submit', $evt);
+        },
+        onKeyUp($evt) {
+            console.log('onKeyUp', $evt);
         }
     }
 }
