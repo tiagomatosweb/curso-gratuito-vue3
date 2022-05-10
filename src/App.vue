4cd2247fca4476a1cbaeac90bbe570e2dab1d539
Tiago Matos <tiago@tiagomatos.com>
2022-05-10 13:35:41 +1000

    aula-7

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
+
+        <h1 :class="{ 'title': true, 'title-home': isHome }">
+            Curso Vue 3
+        </h1>
+
+        <p :class="pClass">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto cumque ea minus qui quo similique sint tempore voluptates. Aliquam consequuntur ea est iste, porro quae ratione. Est facilis sint vel.
+        </p>
+
+        <p :style="styleClass">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, numquam, voluptate. Architecto assumenda commodi cupiditate dolor harum ipsam labore laboriosam mollitia nisi quod repudiandae rerum soluta suscipit vel vero, voluptate.
+        </p>
+
         <div
             v-for="(obj, index) in todos"
-            v-bind:key="obj.id"
+            :key="obj.id"
             class="todos-item"
         >
             <img
@@ -21,6 +34,10 @@ export default {
     name: 'App',
     data() {
         return {
+            isHome: false,
+            classVar: 'title',
+            styleClass: { color: 'aqua', backgroundColor: 'black', 'font-size': '20px' },
+            pClass: ['text', 'text-home'],
             todos: [
                 {
                     "userId": 1,
@@ -61,6 +78,20 @@ export default {
 </script>
 
 <style>
+.title {
+    font-size: 20px;
+    color: blue;
+}
+.title-home {
+    font-size: 40px;
+    color: green;
+}
+.text {
+    color: yellow;
+}
+.text-home {
+    color: #42b983;
+}
 .todos-item {
     background: #000;
     margin: 0 0 5px 0;
